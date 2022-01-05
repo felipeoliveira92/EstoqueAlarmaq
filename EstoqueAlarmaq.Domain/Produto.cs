9f717ee19c971a8e0c677e16b4a823d63f25ff60
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-01-04 21:27:11 -0300

    Initial commit

---
+++ EstoqueAlarmaq.Domain/Produto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueAlarmaq.Domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public Produto(string codigo, string name, string description)
+        {
+            Codigo = codigo;
+            Name = name;
+            Description = description;
+        }
+
+    }
+}
---
+++ EstoqueAlarmaq.Infra/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using EstoqueAlarmaq.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueAlarmaq.Infra.Data
+{
+    public class DataContext : DbContext
+    {
+        DbSet<Produto> Produtos;
+    }
+}
