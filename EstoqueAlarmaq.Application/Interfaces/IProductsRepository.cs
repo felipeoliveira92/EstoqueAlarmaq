c2bf0db5e739c7b5763f5588191826e97de79899
Felipe Rabelo de Oliveira <feliperabelo.oliveira@gmail.com>
2022-02-14 16:43:31 -0300

    implementando a inteface products

---
+++ EstoqueAlarmaq.Application/Interfaces/IProductsRepository.cs
@@ -0,0 +1,22 @@
+﻿using EstoqueAlarmaq.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueAlarmaq.Application.Interfaces
+{
+    public interface IProductsRepository
+    {
+        void Insert(Product product);
+
+        void Update(int id, Product product);
+
+        IEnumerable<Product> SelectAll();
+
+        Product Select(int id);
+
+        void Delete(int id);
+    }
+}
---
+++ EstoqueAlarmaq.Application/Repositories/ProductsRepository.cs
@@ -0,0 +1,47 @@
+﻿using EstoqueAlarmaq.Application.Interfaces;
+using EstoqueAlarmaq.Domain;
+using EstoqueAlarmaq.Infra.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueAlarmaq.Application.Repositories
+{
+    public class ProductsRepository : IProductsRepository
+    {
+        private readonly DataContext _context;
+
+        public void Delete(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+        }
+
+        public void Insert(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+        }
+
+        public Product Select(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            return product;
+        }
+
+        public IEnumerable<Product> SelectAll()
+        {
+            return _context.Products.ToList();
+        }
+
+        public void Update(int id, Product product)
+        {
+            product.Id = id;
+            _context.Update(product);
+            _context.SaveChanges();
+        }
+    }
+}
