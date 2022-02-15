bc6f3993c51a4e54dc6405142fab2202209b7af5
felipeoliveira92 <61589016+felipeoliveira92@users.noreply.github.com>
2022-02-14 22:42:19 -0300

    implementando productsRepository na controller products

--- EstoqueAlarmaq.Api/Controllers/ProductsController.cs
+++ EstoqueAlarmaq.Api/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using EstoqueAlarmaq.Domain;
 using EstoqueAlarmaq.Infra.Data;
+using EstoqueAlarmaq.Services.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,16 +12,19 @@ namespace EstoqueAlarmaq.Api.Controllers
     public class ProductsController : Controller
     {
         private readonly DataContext _context;
+        private readonly ProductsRepository _productsRepository;
 
-        public ProductsController(DataContext context)
+        public ProductsController(DataContext context, ProductsRepository productsRepository)
         {
             _context = context;
+            _productsRepository = productsRepository;
         }
 
         [HttpGet]
         public List<Product> GetProducts()
         {
-            return _context.Products.ToList();
+            //return _context.Products.ToList();
+            return _productsRepository.SelectAll().ToList();
         }
 
         [HttpGet("id")]
