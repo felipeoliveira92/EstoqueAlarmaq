533fa4965383826047df1830abbc1ab09834d36d
Felipe Rabelo de Oliveira <feliperabelo.oliveira@gmail.com>
2022-02-16 15:39:39 -0300

    criado a interface IUserRepository

---
+++ EstoqueAlarmaq.Services/Repositories/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using EstoqueAlarmaq.Domain;
+using EstoqueAlarmaq.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueAlarmaq.Services.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User New()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Select(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> SelectAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
